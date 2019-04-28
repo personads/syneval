db9cc71ff8b0c1315e620ac9bfacf638a67e96ab
Maximilian Müller-Eberstein <work@personads.me>
2019-04-28 17:39:39 +0200

    added final score

--- app/syneval.py
+++ app/syneval.py
@@ -2,7 +2,7 @@ import json, os
 
 from collections import OrderedDict
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, session
 
 from blueprints import auth, examples, evaluation
 
@@ -33,7 +33,19 @@ def create_app():
     # add landing page
     @app.route('/thanks')
     def thanks():
-        return render_template('thanks.html')
+        # retrieve  uid
+        uid = session.get('uid', None)
+        if uid is None:
+            return "Could not retrieve UID.", 500
+        # calculate accuracy
+        results = evaluation.load_user_choices(uid)
+        # check for completion
+        if None in results:
+            accuracy = None
+        else:
+            truths = [task['truth'] for task in app.config['tasks']]
+            accuracy = sum([1 for i in range(len(truths)) if truths[i] == results[i]]) / len(truths)
+        return render_template('thanks.html', accuracy=accuracy)
 
     # configure blueprints
     app.register_blueprint(auth.bp)
